133c6b09f18c88b6376d9cc260b04139875ea15d
Markus Roth <maroth@gmail.com>
2020-03-08 16:59:28 +0100

    tests

--- test_submitter.py
+++ test_submitter.py
@@ -9,10 +9,11 @@ from submitter import Submitter
 from models import Base
 from utils import create_submission
 import config
+import unittest
 
 
-class TestSubmitter:
-    def setup(self):
+class SubmitterTestCase(unittest.TestCase):
+    def setUp(self):
         self.engine = create_engine('sqlite://')
         self.Session = sessionmaker()
         self.Session.configure(bind=self.engine)
@@ -23,39 +24,76 @@ class TestSubmitter:
 
         self.submitter.reddit = Mock()
 
-    def teardown(self):
+    def tearDown(self):
         Base.metadata.drop_all(self.engine)
 
-    def test_submitAllUnsubmitted_unsubmittedSubmission_isSubmitted(self):
+
+class Submit_UnsubmittedSubmission_SubmitCalled_TestCase(SubmitterTestCase):
+    def runTest(self):
+        # Arrange
         unsubmitted = create_submission(submitted=None)
+        submit_response = Mock()
+        submit_response.id(return_value=12)
+        self.submitter.reddit.submit(return_value=submit_response)
         self.submitter.dataAccess.save(unsubmitted)
 
+        # Act
         self.submitter.submit_all_unsubmitted()
 
-        assert self.submitter.reddit.submit.called
+        # Assert
+        self.assertTrue(self.submitter.reddit.submit.called)
 
-    def test_submitAllUnsubmitted_submittedSubmission_isNotSubmitted(self):
+
+class SubmitAllUnsubmitted_NoUnsubmittedSubmission_SubmitNotCalled_TestCase(SubmitterTestCase):
+    def runTest(self):
+        # Arrange
         submitted = create_submission()
         self.submitter.dataAccess.save(submitted)
 
+        # Act
         self.submitter.submit_all_unsubmitted()
 
-        assert not self.submitter.reddit.submit.called
+        # Assert
+        self.assertFalse(self.submitter.reddit.submit.called)
 
-    def test_makeSubmissionTitle_shortValues_concatenatesCorrectly(self):
-        title = self.submitter.make_submission_title(
-            'title', 'description', 100)
-        assert title == 'title ' + config.separator + ' description'
 
-    def test_makeSubmissionTitle_longValues_cutsCorrectly(self):
-        title = self.submitter.make_submission_title(
-            'hans', 'guck in die luft', 12 + len(config.suffix))
-        assert title == 'hans ' + config.separator + ' guck' + config.suffix
+class MakeSubmissionTitle_ShortString_ConcatenatesTitleAndDescription_TestCase(SubmitterTestCase):
+    def runTest(self):
+        # Act
+        title = self.submitter.make_submission_title('title', 'description', 100)
 
-        title = self.submitter.make_submission_title(
-            'hans', 'guck in die luft', 14 + len(config.suffix))
-        assert title == 'hans ' + config.separator + ' guck in' + config.suffix
+        # Assert
+        self.assertEqual('title ' + config.separator + ' description', title)
 
-    def test_makeSubmissionTitle_noDescription_noSuffix(self):
+
+class MakeSubmissionTitle_TooLongString_ShortensToBelowMaxValue_TestCase(SubmitterTestCase):
+    def runTest(self):
+        # Act
+        title = self.submitter.make_submission_title('hans', 'guck in die luft', 12 + len(config.suffix))
+
+        # Assert
+        self.assertEqual('hans ' + config.separator + ' guck' + config.suffix, title)
+
+
+class MakeSubmissionTitle_TooLongString_ShortensToBelowMaxValue_TestCase(SubmitterTestCase):
+    def runTest(self):
+        # Act
+        title = self.submitter.make_submission_title('hans', 'guck in die luft', 14 + len(config.suffix))
+
+        # Assert
+        self.assertEqual('hans ' + config.separator + ' guck in' + config.suffix, title)
+
+
+class MakeSubmissionTitle_NoDescription_NoSuffixAppended_TestCase(SubmitterTestCase):
+    def runTest(self):
+        # Arrange
         title = self.submitter.make_submission_title('hans', '', 100)
-        assert title == 'hans'
+
+        # Act
+
+        # Assert
+        self.assertEqual('hans', title)
+
+
+if __name__ == '__main__':
+        unittest.main()
