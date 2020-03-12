9cf571cede7c9981311a1bccbf544cdad854c771
Markus Roth <maroth@gmail.com>
2020-03-12 23:27:31 +0100

    watson scraper

---
+++ scrapers/watson.py
@@ -0,0 +1,36 @@
+from dataAccess import DataAccess
+from models import Submission
+import requests
+import traceback
+import re
+import os
+from bs4 import BeautifulSoup
+
+
+class WatsonScraper:
+    watson_url = 'https://www.watson.ch'
+
+    def scrape(self):
+        page = requests.get(self.watson_url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        links = ['https://www.watson.ch' + (a.get('href')) for a in soup.find_all("a", class_="teaserlink") if not "doubleclick" in a.get('href')]
+        for link in links:
+            if '/schweiz/' not in link:
+                continue
+            link_page = requests.get(link)
+            link_soup = BeautifulSoup(link_page.content, 'html.parser')
+            title = link_soup.find('h2', class_="maintitle").get_text()
+
+            print ('saving new watson entry: ' + link)
+            try:
+                submission = Submission(
+                    title = title,
+                    description = '',
+                    url = link,
+                    feed_url = self.watson_url,
+                    feed_name= 'Watson')
+                #self.dataAccess.save(submission)
+                print(submission)
+            except:
+                traceback.print_exc()
+
--- tasks.py
+++ tasks.py
@@ -6,6 +6,8 @@ from importer import Importer
 from submitter import Submitter
 from feeds import feeds
 
+from scrapers.watson import WatsonScraper
+
 parser = argparse.ArgumentParser(description='Start chreddit tasks')
 parser.add_argument('taskname', metavar='task_name', help='The name of the task to run')
 args = parser.parse_args()
@@ -13,6 +15,10 @@ args = parser.parse_args()
 if (args.taskname) == 'import':
     importer = Importer()
     importer.import_feeds(feeds)
+
+if (args.taskname) == 'scrape':
+    watson = WatsonScraper()
+    watson.scrape()
 
 if (args.taskname) == 'submit':
     submitter = Submitter()
